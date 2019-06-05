9d17cfc8da8d018425f726a54652c23cf4e60437
ehsan <dr.enyacc@gmail.com>
2019-06-05 13:25:01 +0430

    ref(): edit docker-compose. edit configs.

--- main_config.py
+++ main_config.py
@@ -7,9 +7,9 @@ class BotConfig:
     root_admin = os.environ.get('BOT_ADMIN_ID', '1471278867')
     reports_route = "files/"
     base_url = os.environ.get('BASE_URL', "wss://api.bale.ai/v1/bots/")
-    bot_token = os.environ.get('TOKEN', "4de3376eb96e3c0c4905778ae22065dfefe10b7f")
+    # bot_token = os.environ.get('TOKEN', "4de3376eb96e3c0c4905778ae22065dfefe10b7f")
 
-    # bot_token = os.environ.get('TOKEN', "da5b2817e583c88fa4a736d481693a6fcdbce700")
+    bot_token = os.environ.get('TOKEN', "da5b2817e583c88fa4a736d481693a6fcdbce700")
     system_local = os.environ.get('SYSTEM_LOCAL', "fa_IR")
     resending_max_try = int(os.environ.get('RESENDING_MAX_TRY', 5))
     reuploading_max_try = int(os.environ.get('REUPLOADING_MAX_TRY', 5))
